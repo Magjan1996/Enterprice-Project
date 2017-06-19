7b61388531eac9c799452dc6505e1254b13b0fe0
Magjan <amangeldi144@gmail.com>
2017-06-19 21:37:19 +0600

    first push

---
+++ src/application/beans/LoginControl.java
@@ -0,0 +1,57 @@
+package application.beans;
+
+import org.atmosphere.config.managed.ManagedAtmosphereHandler;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class LoginControl extends HttpServlet implements Serializable {
+
+    private String _LOGIN = "admin";
+    private String _PASSWORD = "admin";
+    private String login;
+    private String pass;
+    /**
+     * sign button click actionMenthod
+     * @return if 'ok' then overdose pages /pages/view.xhtml
+     */
+
+    public String signClick(){
+        if(login.equals(_LOGIN) && pass.equals(_PASSWORD)){
+            return "ok";
+        }else{
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_FATAL, "Не правильные данные",  null);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            return "";
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+}
